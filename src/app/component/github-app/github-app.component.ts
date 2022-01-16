5dc105e2403904072b0e40c511f6b5d920e27514
Nobella-Nyarari-Ejiofor <nobella.ejiofor@student.moringaschool.com>
2022-01-16 17:51:58 -0500

    Added a function for the search bar

--- src/app/component/github-app/github-app.component.ts
+++ src/app/component/github-app/github-app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from 'src/app/github.service';
 import { User } from 'src/app/model/user';
+import { Repositories } from 'src/app/model/repositories';
 
 @Component({
   selector: 'app-github-app',
@@ -10,20 +11,28 @@ import { User } from 'src/app/model/user';
 export class GithubAppComponent implements OnInit {
  
   username = "";
+  repos!: Repositories[];
   
   storedUsers:any ={} ;
 
   constructor(private GithubService: GithubService) { }
 
   ngOnInit(): void {
-    // this.getAccount()
+    this.initialProfile()
   }
 
 
   // getAccount(){
   
   // }
+  initialProfile(){
+    this.username = "Nobella-Nyarari-Ejiofor";
+    this.displayUserName();
+  
+  }
+
   displayUserName(){
+    // this.initialProfile()
     this.GithubService.getUsers().subscribe((data) => {
       
       this.storedUsers = data
@@ -38,6 +47,14 @@ export class GithubAppComponent implements OnInit {
     this.GithubService.getUserName(this.username)
      this.displayUserName()
     //  console.log(this.storedUsers)
+    this.reposGotten()
+  }
+
+  reposGotten(){
+    this.GithubService.getRepos().subscribe((data) => {
+        this.repos = data ;
+        console.log(this.repos)
+    })
   }
 
 };
---
+++ src/app/model/repositories.ts
@@ -0,0 +1,5 @@
+export class Repositories {
+  constructor(public name : string, public description:string, public topics:string){
+
+  }
+}
